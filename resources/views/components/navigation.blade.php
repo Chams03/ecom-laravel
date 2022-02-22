59349cbf370c1713071f2125e5b43f1f0e215659
Chams03 <c.crestfall@yahoo.com.ph>
2022-02-22 17:16:19 +0800

    navigation dev

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,29 @@
+<div class="px-36 p-4 h-20">
+    <div class="flex justify-between">
+        <div class="">
+            <h1>Ecom-Laravel</h1>
+        </div>
+        <div class="flex">
+            <form action="" class="border border-solid border-black">
+                <input class="" type="text" placeholder="Search" id="searchBox">
+            </form>
+            <button type="submit" value="Submit" form="searchBox">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
+        <div>
+            <div>
+                <ul class="flex justify-between">
+                    <li><a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg></a></li>
+                    <li><a href="">Login</a></li>
+                    <li><a href="">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+</div>
